51fdc0de319d8fae711bf14d5fdb9bb3d99b1053
cristianq27 <cris.quezada027@gmail.com>
2025-09-04 11:51:39 -0400

    page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
       <header className="bg-white relative min-h-screen bg-[url(/5fotos.jpg)] bg-no-repeat bg-cover bg-center">
         {/* <img src="/5fotos.jpg" className="absolute inset-0 w-full h-full object-cover"/> */}
         <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 pt-4">
-            <img className="w-56 mx-auto" src="/logopresidencia.svg" alt="Logo Presidencia" />
+            <img className="w-72 mx-auto" src="/logopresidencia.svg" alt="Logo Presidencia" />
           <div className="flex h-16 items-center justify-center">
             <div className="hidden md:block">
               <nav aria-label="Global">
@@ -39,35 +39,41 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="relative z-10 flex flex-col items-center justify-center min-h-screen px-4">
-      <div className="text-center items-center justify-center md:text-left max-w-3xl text-white bg-[#001b39] p-8">
-        <h1 className="text-center text-4xl md:text-6xl font-extrabold mb-0 text-sky-200">
-          5 AÑOS
-        </h1>
-        <p className="text-center text-3xl font-extrabold text-sky-200">
-          CONTRUYENDO
-        </p>
-        <h1 className="text-center text-3xl md:text-7xl font-extrabold mb-0 text-white">
-          OBRAS
-        </h1>
-        <h1 className="text-center text-2xl md:text-4xl font-extrabold mb-0 text-white">
-          QUE CAMBIAN
-        </h1>
-        <h1 className="text-center text-3xl md:text-7xl font-extrabold mb-0 text-white">
-          VIDAS
-        </h1>
-        <div className="flex flex-col md:flex-row items-center md:items-start space-y-4 md:space-y-0 md:space-x-4 bg-white p-8">
-          <h1 className="text-center text-3xl font-extrabold text-bold text-black">
-            ¡Entra aquí
+      <div className="relative z-10 flex flex-col items-center justify-center min-h-screen px-4 position-abusolute right-[450px] mt-[-50px]">
+        <div className="text-center items-center justify-center text-white bg-[#001b39] p-8">
+          <h1 className="text-center text-4xl md:text-6xl font-extrabold mb-0 text-sky-200">
+            5 AÑOS
           </h1>
-          <h1 className="text-center text-3xl font-extrabold text-bold text-black">
-            y conócelas!
+          <p className="text-center text-3xl font-extrabold text-sky-200">
+            CONTRUYENDO
+          </p>
+          <h1 className="text-center text-3xl md:text-7xl font-extrabold mb-0 text-white">
+            OBRAS
           </h1>
-        </div>
-      </div>
+          <h1 className="text-center text-2xl md:text-4xl font-extrabold mb-0 text-white">
+            QUE CAMBIAN
+          </h1>
+          <h1 className="text-center text-3xl md:text-7xl font-extrabold mb-0 text-white">
+            VIDAS
+          </h1>
+          </div>
+          <div className="flex flex-col items-center bg-white p-8">
+            <h1 className="text-center text-5xl font-bold text-bold text-black">
+              ¡Entra aquí
+            </h1>
+            <h1 className=" text-center text-5xl font-bold text-bold text-black mb-10">
+              y conócelas!
+            </h1>
+            <button type="button" className="mb-[-50px]focus:outline-none text-white bg-red-700 hover:bg-red-800 cursor-pointer focus:ring-4 focus:ring-red-300 font-normal text-lg px-5 py-2.5 me-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 w-64 tracking-[10px] items-center text-center">
+              <span>VER MÁS</span>
+              <svg className="w-6 h-6 text-white inline-block ml-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"></path>
+              </svg>
+            </button>
+          </div>
         
-      
-    </div>
+      </div>
+
       </header>
 <div className="max-w-2xl m-auto">
   <div id="default-carousel" className="relative mb-4 mt-4 ml-4" data-carousel="static">
@@ -104,10 +110,10 @@ export default function Home() {
 </div>
       
 
-      <div className="bg-blue-900 flex w-full text-white py-8">
+      <div className="bg-blue-900 flex w-full text-white py-16">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-          <img src="/logopresidencia.svg" alt="Gobierno logo" className="w-40 mx-auto mb-2" />
-          <p className="text-sm">
+          <img src="/logopresidencia-white.svg" alt="Gobierno logo" className="w-72 mx-auto mb-2" />
+          <p className="text-lg mt-6">
             © 2025 Gobierno de la  República Dominicana
           </p>
           <p className="text-xs text-gray-400 mt-2">
